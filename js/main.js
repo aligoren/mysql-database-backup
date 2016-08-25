da8b8f4ba764a4ac8c33686b593cd0e37310b1d6
Ali GOREN <goren.ali@yandex.com>
2016-08-25 11:32:45 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,15 +1,6 @@
 $(function() {
 
     $.btnYedekle = function() {
-        /*
-        serialize fonksiyonu ile formda post edilen tüm ögeler alınır.
-        form#frmLogin kullanıldığı formdaki frmLogin id'sine sahip form'un verilerini seçtirir.
-        dataType json olması bizim için daha işe yarar.
-        success ile post ajax işlemi başarılı ise olacakları belirttik.
-        istek sonucu gelen cevabı yazdırdık.
-        timeout ile belirtilen timeout'a dek ajax çağrısı sonuçlanmazsa hata var demektir. bunun kontrolünü yapabiliriz.
-        post.php kısmında kullanılan sleep(2) fonksiyonu da 2 saniye bekletiyor. yani 2 yerine 3 saniye olursa çağrı sonlanır ve dilersek bir hata yazdırabiliriz.
-        */
         var params = $("form#frmYedekle").serialize();
         $.ajax({
             url: "run.php",
